a3931c418e1bd6928150c937b6b35bd03e939099
BrandonMathis <BeMathis@gmail.com>
2014-01-31 13:26:44 -0500

    Added some film keywords

---
+++ lib/imdbot/keywords.rb
@@ -0,0 +1,30 @@
+module Imdbot
+  class Keywords
+    def self.list
+      [
+        # Alias
+        'Movie',
+        'Film',
+        'Flick',
+        'Theater',
+
+        # Parts
+        'Trailer',
+        'Script',
+        'Score',
+        'Plot',
+
+        # MISC
+        'Directed',
+        'Director',
+        'Starring',
+        'Starred',
+
+        # Television
+        # 'TV',
+        # 'Episode',
+        # 'Episode',
+      ]
+    end
+  end
+end
